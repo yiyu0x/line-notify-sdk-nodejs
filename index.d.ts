9b4f6482859c933a9b522d28ef9b1e224fd74c52
tumornator <doctorKrishanu@gmail.com>
2021-10-16 12:08:00 +0530

    Fixed promises typing

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
-interface Dictionary<T> {
-    [key: string]: T;
-}
-
 declare module "line-notify-sdk" {
+    interface Dictionary<T> {
+        [key: string]: T;
+    }
+    
     export class notifySDK{
         constructor(clientID?:string, clientSecret?:string, redirectURI?:string);
         checkArgsIsSet():void;
         generateOauthURL(state:string,form_post?:boolean):string;
-        getToken(clientCode:string):string|Dictionary<string>;
-        getStatus(tokrn:string):Dictionary<string>;
-        revoke(token:string):Dictionary<string>;
-        notify(token?:string,message?:string, imageThumbnail?:string, imageFullsize?:string, stickerPackageId?:string, stickerId?:string, notificationDisabled?:boolean):Dictionary<string>;
+        getToken(clientCode:string):Promise<string|Dictionary<string>>;
+        getStatus(tokrn:string):Promise<Dictionary<string>>;
+        revoke(token:string):Promise<Dictionary<string>>;
+        notify(token?:string,message?:string, imageThumbnail?:string, imageFullsize?:string, stickerPackageId?:string, stickerId?:string, notificationDisabled?:boolean):Promise<Dictionary<string>>;
     }
 
 }
